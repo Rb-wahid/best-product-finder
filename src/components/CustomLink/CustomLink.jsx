8a1e5b9fe6e93418147c3683263e3f820811cbe7
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2022-04-04 16:00:40 +0600

    customise active link style

--- src/components/CustomLink/CustomLink.jsx
+++ src/components/CustomLink/CustomLink.jsx
@@ -1,20 +1,30 @@
-import React from 'react';
-import { Link, useMatch, useResolvedPath } from 'react-router-dom';
+import React from "react";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
 
 function CustomLink({ children, to, ...props }: LinkProps) {
   let resolved = useResolvedPath(to);
   let match = useMatch({ path: resolved.pathname, end: true });
 
+  let unactiveStyle = {
+    color: "black",
+    paddingBottom: "5px",
+    marginRight: "8px",
+    borderBottom: "4px solid gray",
+  };
+
+  let activeStyle = {
+    color: "rgb(187, 77, 77)",
+    paddingBottom: "5px",
+    marginRight: "8px",
+    borderBottom: "4px solid rgb(163, 44, 44)",
+    fontWeight: "700",
+  };
+
   return (
     <div>
-      <Link
-        style={{ textDecoration: match ? "underline" : "none" }}
-        to={to}
-        {...props}
-      >
+      <Link style={match ? activeStyle : unactiveStyle} to={to} {...props}>
         {children}
       </Link>
-      {match && " (active)"}
     </div>
   );
 }
