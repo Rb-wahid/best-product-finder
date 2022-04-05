6c47c5ec7f56b84486ba7007a1aa038ae38316a2
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2022-04-05 20:28:05 +0600

    final update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
 import Header from "./components/Header/Header";
+import Reviews from "./components/Reviews/Reviews";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Notfound from "./components/Notfound/Notfound";
+import Blog from "./components/Blog/Blog";
 
 function App() {
   return (
-    <>
+    <div className="bg-[#121b2e] text-white text-justify text-xl">
       <Header />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/blogs" element={<Blog />} />
+
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </div>
   );
 }
 
